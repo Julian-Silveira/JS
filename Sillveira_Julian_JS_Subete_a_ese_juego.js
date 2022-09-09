9ce3d957fad5af82feee3974f4b259df7b073cd8
Julian-Silveira <112910100+Julian-Silveira@users.noreply.github.com>
2022-09-08 21:16:09 -0300

    Add files via upload

---
+++ Sillveira_Julian_JS_Subete_a_ese_juego.js
@@ -0,0 +1,7 @@
+var edad = 10 //Se debe tener una edad igual o mayor que 10 
+var altura = 100 //Se debe tener una altura igual o mayor que 100 cm
+console.log(edad, altura)
+
+// Estas variables deben ser configuaradas para que una persona con una 
+// edad mayor o igual que 10 y una altura mayor o igual que 100cm pueda
+// subirse a la montana rusa  
