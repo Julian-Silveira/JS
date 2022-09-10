89034dbb63f065b625d6294cb1d086151a92f49f
Julian-Silveira <112910100+Julian-Silveira@users.noreply.github.com>
2022-09-10 19:18:57 -0300

    Add files via upload

---
+++ Silveira_Julian_Depuracion.js
@@ -0,0 +1,13 @@
+//Usando lo que sabes sobre las funciones, variables, console.log y ahora return, arregla el siguiente código.
+// function greeting(){
+//          return "Hola, mundo";
+//     }
+//     var word = greeting();
+//     console.log();
+function greeting() {
+    return "Hola, mundo";
+}
+var word = greeting();
+console.log(word);
+
+
